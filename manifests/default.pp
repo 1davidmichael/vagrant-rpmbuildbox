bd85af6258019c80ae0c8362597a11ce3741119f
David Michael <1.david.michael@gmail.com>
2014-02-26 09:13:40 -0600

    Initial commit of puppet manifest and vagrant file
    cleaning up manifest

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,14 +1,13 @@
+Yumrepo {
+  enabled  => 1,
+  gpgcheck => 0,
+}
+
 yumrepo {
   'spacewalk':
-    baseurl  => 'http://yum.spacewalkproject.org/2.0/RHEL/6/$basearch/',
-    enabled  => 1,
-    gpgcheck => 0,
-    gpgkey   => 'http://yum.spacewalkproject.org/RPM-GPG-KEY-spacewalk-2012';
+    baseurl  => 'http://yum.spacewalkproject.org/2.0/RHEL/6/$basearch/';
   'epel6':
-    mirrorlist => 'https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=$basearch',
-    enabled    => 1,
-    gpgcheck   => 0,
-    gpgkey     => 'https://fedoraproject.org/static/A4D647E9.txt';
+    mirrorlist => 'https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=$basearch';
 }
 
 Package {
